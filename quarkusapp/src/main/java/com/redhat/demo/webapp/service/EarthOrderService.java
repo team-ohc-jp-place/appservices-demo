62c5b2ad4dfaec68d47d06507ec7b931a1ef73cc
kamorisan <kamori@redhat.com>
2021-12-13 15:21:24 +0900

    update

--- quarkusapp/src/main/java/com/redhat/demo/webapp/service/EarthOrderService.java
+++ quarkusapp/src/main/java/com/redhat/demo/webapp/service/EarthOrderService.java
@@ -21,6 +21,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 @ApplicationScoped
 public class EarthOrderService {
@@ -30,15 +31,14 @@ public class EarthOrderService {
     @Inject
     EventBus eventBus;
 
-
-
     @Transactional(value = TxType.MANDATORY)
     public void orderCreated(JsonNode event) throws JsonMappingException, JsonProcessingException {
         
 
         LOGGER.info("event: '{}'", event);
 
-
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
 
 
 
