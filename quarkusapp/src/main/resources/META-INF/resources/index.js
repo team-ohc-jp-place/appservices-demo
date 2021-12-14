fe739716f1602a9fa6693756f98913ffb3bf011c
kamorisan <kamori@redhat.com>
2021-12-14 17:52:22 +0900

    update

--- quarkusapp/src/main/resources/META-INF/resources/index.js
+++ quarkusapp/src/main/resources/META-INF/resources/index.js
@@ -3,7 +3,7 @@ if (!!window.EventSource) {
     eventSource.onmessage = function(event) {
         data = JSON.parse(event.data);
 
-        console.log(data);
+        //console.log(data);
 
         //var row = '<tr><td>' + data.accountId + '</td><td>' + data.orderId + '</td><td>' + data.orderType + '</td><td>' + data.orderItemName + '</td><td>' + data.openDate + '</td><td>' + data.symbol + '</td><td style="text-align: right;">' + data.price + '</td><td style="text-align: right;">' + data.orderFee + '</td></tr>';
         var row = '<tr><td>' + data.orderId + '</td><td>' + data.orderType + '</td><td>' + data.orderItemName + '</td><td>' + data.quantity + '</td><td>' + data.price + '</td><td>' + data.shipmentAddress + '</td><td>' + data.zipCode + '</td><td>' + data.totalAmount + '</td><td>' + data.deliveryFee + '</td><td>' + data.stateCode + '</td><td>' + data.stateName + '</td></tr>';
